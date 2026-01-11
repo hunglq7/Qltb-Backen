4f2d0311ae26b587e23719b3ce53cfe8eb50d159
Le Quang Hung <hunglq7@gmail.com>
2026-01-11 14:51:08 +0700

    11.01.2026-14h51

--- obj/Debug/net7.0-windows/WebApi.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0989b544b465e6f3302cf00686fc47cbaf119cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d720fad4165262d18774f5bd4a086aceccd82f9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
